196f1d19b13fdb63e3fe1c6e1e61b458d8849203
Safwan Backar <safwanbackar29@gmail.com>
2022-07-18 19:01:20 +0530

    firebase quote data

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,38 +1,58 @@
-import { Fragment } from "react"
-import { Link, Route, useParams, useRouteMatch } from "react-router-dom"
-import Comments from '../components/comments/Comments'
-import HighlightedQuote from '../components/quotes/HighlightedQuote'
+import { Fragment, useEffect } from 'react';
+import { useParams, Route, Link, useRouteMatch } from 'react-router-dom';
 
-const DUMMY_QUOTES = [
-    {id: 'q1', author: 'Max', text: 'React'},
-    {id: 'q2', author: 'madMax', text: 'Javascript'}
-]
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
+import Comments from '../components/comments/Comments';
+import useHttp from '../hooks/use-http';
+import { getSingleQuote } from '../lib/api';
+import LoadingSpinner from '../components/UI/LoadingSpinner';
 
-const QuoteDetail = () =>{
-    const match = useRouteMatch()
-    const params = useParams()
-    const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId)
+const QuoteDetail = () => {
+  const match = useRouteMatch();
+  const params = useParams();
 
-    if(!quote){
-        return <p>No quote found</p>
-    }
+  const { quoteId } = params;
 
+  const { sendRequest, status, data: loadedQuote, error } = useHttp(
+    getSingleQuote,
+    true
+  );
 
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  if (status === 'pending') {
     return (
-    <Fragment>
-    <HighlightedQuote text={quote.text} author={quote.author}/>
-      <Route path={`${match.path}`} exact>
-       <div className="centered">
-        <Link className="btn--flat" to={`${match.url}/comments`}>
-            Load Comments
-        </Link>
+      <div className='centered'>
+        <LoadingSpinner />
       </div>
-    </Route>
-    <Route path={`${match.path}/comments`}>
-        <Comments/>
-    </Route>
-    </Fragment>
-    )
-}
+    );
+  }
 
-export default QuoteDetail
+  if (error) {
+    return <p className='centered'>{error}</p>;
+  }
+
+  if (!loadedQuote.text) {
+    return <p>No quote found!</p>;
+  }
+
+  return (
+    <Fragment>
+      <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
+      <Route path={match.path} exact>
+        <div className='centered'>
+          <Link className='btn--flat' to={`${match.url}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
+      <Route path={`${match.path}/comments`}>
+        <Comments />
+      </Route>
+    </Fragment>
+  );
+};
+
+export default QuoteDetail;
